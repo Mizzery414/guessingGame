f686633405dfdf412cec16c46791196f8da07b2b
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2021-11-24 16:52:37 -0800

    update js file to add logic

--- script.js
+++ script.js
@@ -3,18 +3,28 @@
 console.log(document.querySelector(".message").textContent);
 document.querySelector(".message").textContent = "🎉 Correct Number!";
 
-document.querySelector(".number").textContent = 13;
+document.querySelector(".secretnumber").textContent = 13;
 document.querySelector(".score").textContent = 10;
 
 document.querySelector(".guess").value = 23;
 console.log(document.querySelector(".guess").value);
-
 */
+
+const secretNumber = Math.ceil(Math.random() * 20);
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No Number!";
+  } else if (guess === secretNumber) {
+    document.querySelector(".message").textContent = "🎉 Correct Number!";
+  } else if (guess > secretNumber) {
+    document.querySelector(".message").textContent = "📈 Too High!";
+  } else if (guess < secretNumber) {
+    document.querySelector(".message").textContent = "📉 Too Low!";
   }
 });
