f9026325fed0a53d706b83780a06f579f484f4b0
Melanie Rowsey-McWilliams <melanie.rowsey@gmail.com>
2021-11-24 21:14:37 -0800

    add if/else statements for score and highscore, change css background color on correct guess

--- script.js
+++ script.js
@@ -18,13 +18,37 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔️ No Number!";
+
+    //when player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+
+    //when guess is too high
   } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "📈 Too High!";
+    if (score > 1) {
+      document.querySelector(".message").textContent = "📈 Too High!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "🤯 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
+
+    //when guess is too low
   } else if (guess < secretNumber) {
-    document.querySelector(".message").textContent = "📉 Too Low!";
+    if (score < 20) {
+      document.querySelector(".message").textContent = "📉 Too Low!";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "🤯 You lost the game!";
+      document.querySelector(".score").textContent = 0;
+    }
   }
 });
